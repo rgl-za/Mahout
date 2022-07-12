45db71890615d0e21c5e28993190b504d1be108d
rgl-za <hyeonjeong2829@gmail.com>
2022-07-12 19:03:22 +0900

    스프링배치 연동, csv 파일 생성

--- mahout/src/main/java/com/example/demo/MahoutApplication.java
+++ mahout/src/main/java/com/example/demo/MahoutApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MahoutApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		SpringApplication.run(MahoutApplication.class, args);
 	}
 
---
+++ mahout/src/main/java/com/example/demo/domain/RecommendDTO.java
@@ -0,0 +1,10 @@
+package com.example.demo.domain;
+
+import lombok.Data;
+
+@Data
+public class RecommendDTO {
+    private int userNo;
+    private int pnum;
+    private int prefer;
+}
