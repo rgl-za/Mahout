e37f156d75b32d968c061dff789a722af541a9fc
rgl-za <hyeonjeong2829@gmail.com>
2022-09-26 23:08:17 +0900

    no message

--- mahout/src/main/java/com/example/demo/controller/RecommendController.java
+++ mahout/src/main/java/com/example/demo/controller/RecommendController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import lombok.var;
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.impl.common.LongPrimitiveIterator;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
@@ -11,9 +12,13 @@ import org.junit.Test;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
@@ -21,19 +26,29 @@ public class RecommendController {
 
     @GetMapping("/")
     public void itemRecommend(){
+        Long pnum = 2L;
         try{
             DataModel dm = new FileDataModel(new File("/Users/jihyeonjeong/Mahout/mahout/data"));
             TanimotoCoefficientSimilarity sim = new TanimotoCoefficientSimilarity(dm);
             GenericItemBasedRecommender recommender = new GenericItemBasedRecommender(dm, sim);
             int x = 1;
-            for (LongPrimitiveIterator items = dm.getItemIDs(); items.hasNext();) {
-                long itemId = items.nextLong();
+             for (LongPrimitiveIterator items = dm.getItemIDs(); items.hasNext();) {
+                 long itemId = items.nextLong();
+
                 List<RecommendedItem> recommendations = recommender.mostSimilarItems(itemId, 5);
-                for (RecommendedItem recommendation : recommendations) {
-                    System.out.println(itemId + "," + recommendation.getItemID() + "," + recommendation.getValue());
+                if(pnum == itemId){
+                    for (RecommendedItem recommendation : recommendations) {
+                        // System.out.println(itemId + "," + recommendation.getItemID() + "," + recommendation.getValue());
+                        System.out.println(recommendation.getItemID());
+                    }
                 }
-                x++;
+
+
                 // if(x> 10) System.exit(1);
+//                 if(pnum.equals(recommend.keySet())){
+//                     System.out.println(recommend.values());
+//
+//                 }
             }
         } catch (IOException e) {
             System.out.println("there was an error.");
---
+++ mahout/src/main/java/com/example/demo/domain/PostDTO.java
@@ -0,0 +1,70 @@
+package com.example.demo.domain;
+
+import java.time.LocalDateTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+public class PostDTO {
+    // 게시글 고유 번호
+    private Long pnum;
+
+    // 카테고리 고유 번호
+    private Long catnum;
+
+    // 방장 아이디
+    private String leaderid;
+
+    // 게시글 제목
+    private String title;
+
+    // 게시글 내용
+    private String content;
+
+    // 사진 업로드
+    private String photo;
+
+    // 제품 링크
+    private String link;
+
+    // 제품 원가
+    private int price;
+
+    // 우편번호
+    private int postnum;
+
+    // 거래 주소
+    private String dealaddress;
+
+    // 최대 인원
+    private int maxpeople;
+
+    // 최소 인원
+    private int minpeople;
+
+    // 게시글 전용 계좌
+    private String accountpost;
+
+    // 게시글 업로드 날짜
+    private LocalDateTime uploaddate;
+
+    // 삭제 키
+    private String deleteyn;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    private LocalDateTime deletetime;
+
+    // 게시글 거래 마감 날짜
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    private LocalDateTime enddate;
+
+    // 거래가 끝난후 방장이 end버튼을 눌렀을때 입력되는 날짜 데이터
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    private LocalDateTime finaldate;
+
+    // 물품 개수
+    private Long numbers;
+
+}
